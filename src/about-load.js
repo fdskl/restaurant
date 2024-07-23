8edd69d9cba4fda19f3fe715d420644d7fc7006b
fdskl <6555057+fdskl@users.noreply.github.com>
2024-07-23 20:50:21 +0100

    Add content to About page

--- src/about-load.js
+++ src/about-load.js
@@ -8,6 +8,22 @@ const aboutLoad = () => {
     header1.innerText = "ABOUT"
     x.append(header1);
 
+    let header2 = document.createElement("h2");
+    header2.innerText = "Address"
+    x.append(header2);
+
+    let paragraph1 = document.createElement("p");
+    paragraph1.innerText = "742 Evergreen Street, Sprungfeld, Texas, TX0986"
+    x.append(paragraph1); 
+
+    let header3 = document.createElement("h2");
+    header3.innerText = "Phone number"
+    x.append(header3);
+
+    let paragraph2 = document.createElement("p");
+    paragraph2.innerText = "+01 789 345 4000"
+    x.append(paragraph2); 
+
 }
 
 export default aboutLoad;
